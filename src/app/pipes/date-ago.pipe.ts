f9d03c933705eb90432adca51149aee94fc02c94
Martin Cardenas <a1martinca11@gmail.com>
2023-12-23 18:49:37 -0700

    Fixed issue where date of a just created post was not formatted

--- src/app/pipes/date-ago.pipe.ts
+++ src/app/pipes/date-ago.pipe.ts
@@ -5,51 +5,54 @@ import { Pipe, type PipeTransform } from '@angular/core'
   standalone: true
 })
 export class DateAgoPipe implements PipeTransform {
-  transform (date: string | undefined): string | undefined {
-    if (date !== undefined) {
-      const dateFromDatabase = new Date(date)
-      const userTimezoneOffset = dateFromDatabase.getTimezoneOffset() * 60000
-      const dateLocal = new Date(
-        dateFromDatabase.getTime() - userTimezoneOffset
+  transform (date: string | null): string {
+    if (date === null) {
+      return ''
+    }
+
+    const dateFromDatabase = new Date(date)
+    const userTimezoneOffset = dateFromDatabase.getTimezoneOffset() * 60000
+    const dateLocal = new Date(
+      dateFromDatabase.getTime() - userTimezoneOffset
+    )
+
+    const utcNow = new Date(
+      Date.UTC(
+        new Date().getUTCFullYear(),
+        new Date().getUTCMonth(),
+        new Date().getUTCDate(),
+        new Date().getUTCHours(),
+        new Date().getUTCMinutes(),
+        new Date().getUTCSeconds()
       )
+    )
 
-      const utcNow = new Date(
-        Date.UTC(
-          new Date().getUTCFullYear(),
-          new Date().getUTCMonth(),
-          new Date().getUTCDate(),
-          new Date().getUTCHours(),
-          new Date().getUTCMinutes(),
-          new Date().getUTCSeconds()
-        )
-      )
+    const seconds = Math.floor(
+      (utcNow.getTime() - dateLocal.getTime()) / 1000
+    )
 
-      const seconds = Math.floor(
-        (utcNow.getTime() - dateLocal.getTime()) / 1000
-      )
-      if (seconds < 29 && seconds > 0) {
-        return 'Just now'
-      }
+    if (seconds < 29) {
+      return 'Just now'
+    }
 
-      const intervals: Record<string, number> = {
-        year: 31536000,
-        month: 2592000,
-        week: 604800,
-        day: 86400,
-        hour: 3600,
-        minute: 60,
-        second: 1
-      }
+    const intervals: Record<string, number> = {
+      year: 31536000,
+      month: 2592000,
+      week: 604800,
+      day: 86400,
+      hour: 3600,
+      minute: 60,
+      second: 1
+    }
 
-      let counter: number
-      for (const i in intervals) {
-        counter = Math.floor(seconds / intervals[i])
-        if (counter > 0) {
-          if (counter === 1) {
-            return counter + ' ' + i + ' ago'
-          } else {
-            return counter + ' ' + i + 's ago'
-          }
+    let counter: number
+    for (const i in intervals) {
+      counter = Math.floor(seconds / intervals[i])
+      if (counter > 0) {
+        if (counter === 1) {
+          return counter + ' ' + i + ' ago'
+        } else {
+          return counter + ' ' + i + 's ago'
         }
       }
     }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,9 +1,9 @@
-import { Component, Inject } from '@angular/core'
+import { Component, Inject, type OnInit } from '@angular/core'
 import { ActivatedRoute, RouterLink } from '@angular/router'
 import { GetPostService } from './get-post.service'
 import { CommonModule, NgOptimizedImage } from '@angular/common'
 import { type IPost } from '../posts/IPost'
-import { type Observable } from 'rxjs'
+import { Observable } from 'rxjs'
 import { DateAgoPipe } from '../pipes/date-ago.pipe'
 
 @Component({
@@ -13,13 +13,17 @@ import { DateAgoPipe } from '../pipes/date-ago.pipe'
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
-export class PostComponent {
-  post$: Observable<IPost> = this.getPostService.getPost(
-    this.route.snapshot.params['id'] as number
-  )
+export class PostComponent implements OnInit {
+  post$: Observable<IPost> = new Observable<IPost>()
 
   constructor (
     @Inject(ActivatedRoute) private readonly route: ActivatedRoute,
     @Inject(GetPostService) private readonly getPostService: GetPostService
   ) {}
+
+  ngOnInit (): void {
+    this.post$ = this.getPostService.getPost(
+      this.route.snapshot.params['id'] as number
+    )
+  }
 }
